0c743ef4114489bfed5cd9c4dffc56f562873134
totembeing <v.ibhu27005@gmail.com>
2024-08-01 14:07:02 -0400

    Made the about page and added responsiveness. Have to redo the landing page.

--- JS/index.js
+++ JS/index.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
     $(function() {
         $("#head").load("meta.html");
         $("#header").load("header.html");
+        $("#footer").load("footer.html");
     });
 });
